2ac5bf87fccfa434f7f946aea9d1ffc1eb323d60
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-06-22 16:43:16 +0100

    add redux, store and provider

--- repo_recreator/src/index.js
+++ repo_recreator/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import repoReducer from './reducers/repoReducer';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import './index.css';
+
 import App from './App';
+const store = createStore(repoReducer, devToolsEnhancer())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
